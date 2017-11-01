2511de6a5945176b84361de9259df82df50ae435
Roman Pogribnyi <pogribnyi@gmail.com>
2017-11-01 15:10:50 +0100

    Initialize the NN with some args

---
+++ neural_network.py
@@ -0,0 +1,26 @@
+"""
+Simple implementation of a neural network
+"""
+import numpy as np
+
+def sigmoid(value):
+    pass
+
+class NeuralNetwork(object):
+    def __init__(self, input_layer_size, hidden_layer_size, output_layer_size):
+        """Initialize the NN
+
+        Args:
+            input_layer_size (int): size of the input dimention
+            hidden_layer_size (int): size of the hidden layer
+            output_layer_size (int): size of the output dimention
+        """
+        self.input_layer_size = input_layer_size
+        self.hidden_layer_size = hidden_layer_size
+        self.output_layer_size = output_layer_size
+
+    def forward(self):
+        """ Propagate the values through the network
+        """
+        pass
+ 
